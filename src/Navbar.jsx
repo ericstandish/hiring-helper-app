aa1b4cca9fac3edf2a109dd3eacd24f4a3eb06ad
Eric Standish <ericstandish12@gmail.com>
2024-03-28 14:58:07 -0500

    navbar modifications

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.pageYOffset;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="bg-teal-500 p-4">
+    <nav className={`fixed top-0 w-full z-10 ${scrollPosition > 0 ? "bg-teal-500 navbar-solid transition-all duration-300" : "bg-transparent navbar-transparent transition-all duration-300"} py-3`}>
       <div className="max-w-7xl mx-auto flex justify-between items-center saira-1">
         <div className="flex items-center">
           <Link to="/" className="flex items-center">
-            <img src="/favicon.ico" alt="favicon" className="h-8 mr-4" />
+            <img src="/favicon.ico" alt="favicon" className="h-12 mr-4" />
           </Link>
           <ul className="flex space-x-4">
             <li>
               <Link
                 to="/"
-                className="text-white hover:text-orange-100 transition duration-300 ease-in-out"
+                className="px-4 py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none"
               >
                 Home
               </Link>
@@ -21,7 +36,7 @@ function Navbar() {
             <li>
               <Link
                 to="/ahptool"
-                className="text-white hover:text-orange-100 transition duration-300 ease-in-out"
+                className="px-4 py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none"
               >
                 AHP Tool
               </Link>
@@ -29,7 +44,7 @@ function Navbar() {
             <li>
               <Link
                 to="/example"
-                className="text-white hover:text-orange-100 transition duration-300 ease-in-out"
+                className="px-4 py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none"
               >
                 Example
               </Link>
@@ -37,7 +52,7 @@ function Navbar() {
           </ul>
         </div>
         <div>
-          <button className="px-4 py-2 rounded-lg bg-white text-orange-500 hover:bg-orange-100 transition duration-300 ease-in-out focus:outline-none">
+          <button className="px-4 py-2 rounded-full bg-orange-500 text-white hover:bg-orange-600 transition duration-300 ease-in-out focus:outline-none">
             Sign In
           </button>
         </div>
