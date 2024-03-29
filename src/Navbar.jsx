b7e47ef92164cc6be1e6ecc4d674c720324d2840
Eric Standish <ericstandish12@gmail.com>
2024-03-28 19:14:45 -0500

    mobile view revision

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -22,37 +22,34 @@ function Navbar() {
       <div className="max-w-7xl mx-auto flex justify-between items-center saira-1">
         <div className="flex items-center">
           <Link to="/" className="flex items-center">
-            <img src="/favicon.ico" alt="favicon" className="h-12 md:h-20 mr-4 md:mr-24" /> {/* Adjusted height and margin for mobile view */}
+            <img src="/favicon.ico" alt="favicon" className="h-12 md:h-20 mr-4 md:mr-24" />
           </Link>
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-2 md:space-x-4"> {/* Reduced space between items for mobile view */}
             <li>
               <Link
                 to="/"
-                className="px-4 py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none"
-              >
+                className="px-2 py-1 md:px-4 md:py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none text-sm md:text-base">
                 Home
               </Link>
             </li>
             <li>
               <Link
                 to="/ahptool"
-                className="px-4 py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none"
-              >
+                className="px-2 py-1 md:px-4 md:py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none text-sm md:text-base">
                 AHP Tool
               </Link>
             </li>
             <li>
               <Link
                 to="/example"
-                className="px-4 py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none"
-              >
+                className="px-2 py-1 md:px-4 md:py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none text-sm md:text-base">
                 Example
               </Link>
             </li>
           </ul>
         </div>
         <div>
-          <button className="px-4 py-2 rounded-full bg-orange-500 text-white hover:bg-orange-600 transition duration-300 ease-in-out focus:outline-none">
+          <button className="px-2 py-1 md:px-4 md:py-2 rounded-full bg-orange-500 text-white hover:bg-orange-600 transition duration-300 ease-in-out focus:outline-none text-sm md:text-base"> {/* Adjusted padding and font size */}
             Sign In
           </button>
         </div>
