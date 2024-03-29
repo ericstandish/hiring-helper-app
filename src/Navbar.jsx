6bc6d3e245a96300ba8fd6b6585a10475e6e3e80
Eric Standish <ericstandish12@gmail.com>
2024-03-28 23:50:47 -0500

    take the user to the top of the page on link click

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleScroll = () => {
-      const position = window.pageYOffset;
+      const position = window.scrollY;
       setScrollPosition(position);
     };
 
@@ -17,17 +19,22 @@ function Navbar() {
     };
   }, []);
 
+  const handleLinkClick = () => {
+    window.scrollTo(0, 0); // Scroll to the top of the page
+  };
+
   return (
     <nav className={`fixed top-0 w-full z-10 ${scrollPosition > 0 ? "bg-teal-400 navbar-solid transition-all duration-300" : "bg-transparent navbar-transparent transition-all duration-300"} py-3`}>
       <div className="max-w-7xl mx-auto flex justify-between items-center saira-1">
         <div className="flex items-center">
-          <Link to="/" className="flex items-center">
+          <Link to="/" onClick={handleLinkClick} className="flex items-center">
             <img src="/favicon.ico" alt="favicon" className="h-12 md:h-20 mr-4 md:mr-24" />
           </Link>
           <ul className="flex space-x-2 md:space-x-4"> {/* Reduced space between items for mobile view */}
             <li>
               <Link
                 to="/"
+                onClick={() => { handleLinkClick(); navigate('/'); }}
                 className="px-2 py-1 md:px-4 md:py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none text-sm md:text-base">
                 Home
               </Link>
@@ -35,6 +42,7 @@ function Navbar() {
             <li>
               <Link
                 to="/ahptool"
+                onClick={() => { handleLinkClick(); navigate('/ahptool'); }}
                 className="px-2 py-1 md:px-4 md:py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none text-sm md:text-base">
                 AHP Tool
               </Link>
@@ -42,6 +50,7 @@ function Navbar() {
             <li>
               <Link
                 to="/example"
+                onClick={() => { handleLinkClick(); navigate('/example'); }}
                 className="px-2 py-1 md:px-4 md:py-2 rounded-full bg-orange-500 text-white hover:text-orange-100 transition duration-300 ease-in-out focus:outline-none text-sm md:text-base">
                 Example
               </Link>
